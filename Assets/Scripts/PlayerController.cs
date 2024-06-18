f8111e302dffcadb719dd6fde9d776e546e2b5e2
José Magalhães <joses.magalhaes@hotmail.com>
2024-06-18 20:45:27 -0300

    :construction: Criação do life do player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float velocidade = 5f;
     private Rigidbody2D rb;
+    public Animator playerAnimator;
+    public Slider lifeSlider;
+    public float velocidade = 5f;
+    bool isWalking = false;
+    public int playerHealth = 100;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        isWalking = false;
+        Debug.Log("Life do Player: " + playerHealth);
     }
 
     // Update is called once per frame
@@ -17,8 +25,18 @@ public class PlayerController : MonoBehaviour
     {
         float eixoX = Input.GetAxisRaw("Horizontal") * velocidade;
         float eixoY = Input.GetAxisRaw("Vertical") * velocidade;
+        isWalking = eixoX != 0 || eixoY != 0;
 
         rb.velocity = new Vector2(eixoX, eixoY);
         Debug.Log($"Horizontal: {eixoX}, Vertical: {eixoY}");
+
+        if (isWalking)
+        {
+            playerAnimator.SetFloat("eixoX", eixoX);
+            playerAnimator.SetFloat("eixoY", eixoY);
+        }
+
+        playerAnimator.SetBool("isWalking", isWalking);
+        lifeSlider.value = playerHealth * 0.01f;
     }
 }
