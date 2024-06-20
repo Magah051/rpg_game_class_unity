60605684a4ce72bbaf0f6e81fc80091ba1818494
José Magalhães <joses.magalhaes@hotmail.com>
2024-06-20 00:25:06 -0300

    :construction: Finalização de IA do inimigo

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -19,12 +19,57 @@ public class EnemyController : MonoBehaviour
 
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        currentTarget = waypointA;
+        scale = transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveTowardsTarget();
+    }
+
+    private void MoveTowardsTarget()
+    {
+        Vector3 curTargetHorizontal = new Vector2(currentTarget.position.x, transform.position.y);
+        Vector2 direction = (curTargetHorizontal - transform.position).normalized;
+
+        transform.position += (Vector3)direction * movementeSpeed * Time.deltaTime;
+
+        if (Vector2.Distance(curTargetHorizontal, transform.position) <= 0.2f)
+        {
+            SwitchTarget();
+        }
+        UpdateAnimation();
+
+    }
+
+    private void SwitchTarget()
+    {
+        if (currentTarget == waypointA)
+        {
+            currentTarget = waypointB;
+            Flip();
+        }
+        else
+        {
+            currentTarget = waypointA;
+            transform.localScale = scale;
+        }
+    }
+
+    private void UpdateAnimation()
+    {
+        isWalking = (Vector2.Distance(transform.position, currentTarget.position) > 0.1f);
+        animator.SetBool("isWalking", isWalking);
+    }
+
+    private void Flip()
+    {
+        Vector3 flippedScale = scale;
+        flippedScale.x *= -1;
+        transform.localScale = flippedScale;
     }
 }
